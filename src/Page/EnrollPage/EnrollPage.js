15acce9105a1a74b7e42ec554c49528620b4aec1
Max <mchen210@uottawa.ca>
2020-11-21 00:45:21 -0500

    Enrollment workflow refactor

--- src/Page/EnrollPage/EnrollPage.js
+++ src/Page/EnrollPage/EnrollPage.js
@@ -5,12 +5,10 @@ import ReactDOM from 'react-dom';
 import CameraModule from '../../Components/CameraModule/CameraModule';
 
 export default class EnrollPage extends Component {
-    
-    state =  {personName: '', sentCreationRequest: false, personId:''}
-
+    state = {perosnName:'', personId:''}
     constructor(props) {
-        super(props) 
-        this.addFace = this.addFace.bind(this)
+        super(props);
+        this.enrollment = this.enrollment.bind(this);
     }
 
     handleChange = (e) => {
@@ -33,21 +31,53 @@ export default class EnrollPage extends Component {
         }
     }
     
-    addFace = (childData) =>{
-        console.log(childData);
-       
-    }
-
     personResponseHandler(res){
-        this.setState.personId = res.personId;
+        this.setState({personId: res.personId})
         const newElement = (
             <div className = 'pageBody'>
                 <h1 className = 'greetTxt'> Hi 👋{this.state.personName},  please show me your face</h1>
                 <h1 className = 'greetTxt'> Please hold still</h1>
-                <CameraModule addFace = {this.addFace}/>
+                <CameraModule enrollment = {this.enrollment} isEnroll = {true} personId = {this.state.personId}/>
             </div>
         )
         ReactDOM.render(newElement, document.getElementById('root'))
+    }
+
+    enrollment(res){
+        if (res.length !== 0){
+            this.trainPersonGroup();
+        }else{
+            const newElement = (
+            <div className = 'pageBody'>
+                <h1 className = 'greetTxt'> Hi 👋{this.state.personName},  I cannot see your face</h1>
+                <h1 className = 'greetTxt'> Please hold still and show me again</h1>
+                <CameraModule  isEnroll = {true} personId = {this.state.personId}/>
+            </div>
+            )
+            ReactDOM.render(newElement, document.getElementById('root'))
+        }
+    }
+
+    trainPersonGroup(){
+        fetch(process.env.REACT_APP_API_ENDPOINT_TRAIN, {
+            method: 'post',
+            headers: {
+                "Ocp-Apim-Subscription-Key": process.env.REACT_APP_API_KEY
+            }
+        }).then(this.getTrainStatus())
+          .catch(err => console.log('Train_Request Fail', err));
+    }
+
+    getTrainStatus(){
+        let status = '';
+       fetch(process.env.REACT_APP_API_ENDPOINT_STATUS, {
+            method: 'get',
+            headers: {
+                "Ocp-Apim-Subscription-Key": process.env.REACT_APP_API_KEY
+            }
+        }).then(res => {status = res.status;
+                        console.log(status);})
+        .catch(err => console.log('Get_Training_Status Request Fail', err));
     }
 
     render() {
--- src/Page/VerifyPage/VerifyPage.js
+++ src/Page/VerifyPage/VerifyPage.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import CameraModule from '../../Components/CameraModule/CameraModule';
 
 export default class VerifyPage extends Component {
     render() {
         return (
-            <div>
-                <h3>looolkdasf</h3>
+            <div className = 'pageBody'>
+                <h1 className = 'greetTxt'> Hi 👋,  I am trying to recognize you</h1>
+                <h1 className = 'greetTxt'> Please hold still</h1>
+                <CameraModule />
             </div>
         )
     }
